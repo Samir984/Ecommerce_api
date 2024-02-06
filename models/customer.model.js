762f60cb229bed65dbd724ba7f8e97821ff50057
samir984 <sasuki984@gmail.com>
2024-02-06 22:22:59 +0545

    add  customer model

--- models/customer.model.js
+++ models/customer.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const customerSchema = mongoose.Schema({
   fullName: {
@@ -29,14 +30,22 @@ const customerSchema = mongoose.Schema({
   },
 });
 
-const Customer = mongoose.model("Customer", customerSchema);
-
 customerSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash();
   next();
 });
 
-customerSchema.methods={
-    
-}
+customerSchema.methods = {
+  comparePassword: async function (plainTextPassword) {
+    return await bcrypt.compare(plainTextPassword, this, password);
+  },
+
+  generateAccessToken: async function () {
+    return jwt.sign({ _id: this._id }, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: process.env.ACCESS_TOKEN_SECRET,
+    });
+  },
+};
+
+export const Customer = mongoose.model("Customer", customerSchema);
